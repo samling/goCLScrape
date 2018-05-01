19dee21cfd54fad008768ab24d9c7f65f96173f6
Sam Boynton <samlingx@gmail.com>
2018-04-30 18:44:11 -0700

    Working query formulation with gorilla and proper yaml unmarshaling

--- src/main.go
+++ src/main.go
@@ -5,35 +5,61 @@ import (
     "io/ioutil"
     "log"
     "net/url"
-    //"strings"
 
+    "github.com/gorilla/Schema"
     "gopkg.in/yaml.v2"
 )
 
 type Config struct {
     Search struct {
-        URL     string  `yaml:"URL"`
+        Scheme              string  `yaml:"Scheme"`
+        Location            string  `yaml:"Location"`
+        URL                 string  `yaml:"URL"`
     } `yaml:"Search"`
 
     SMTP struct {
-        Host    string  `yaml:"host,omitempty"`
-        Port    string  `yaml:"port,omitempty"`
-        User    string  `yaml:"user,omitempty"`
-        Pass    string  `yaml:"pass,omitempty"`
-    } `yaml:"SMTP,omitempty"`
+        Host                string  `yaml:"host,omitempty"`
+        Port                string  `yaml:"port,omitempty"`
+        User                string  `yaml:"user,omitempty"`
+        Pass                string  `yaml:"pass,omitempty"`
+    } `yaml:"SMTP"`
 
     Query struct {
-        HasPic  string  `yaml:"hasPic"`
-    } `yaml:"Query,omitempty"`
+        Format              string   `yaml:"format,omitempty"`
+        HasPic              string   `yaml:"hasPic,omitempty"`
+        SrchType            string   `yaml:"srchType,omitempty"`
+        BundleDuplicates    string   `yaml:"bundleDuplicates,omitempty"`
+        MinPrice            string   `yaml:"min_price,omitempty"`
+        MaxPrice            string   `yaml:"max_price,omitempty"`
+        PostedToday         string   `yaml:"postedToday,omitempty"`
+        SaleDate            string   `yaml:"sale_date,omitempty"`
+        AvailabilityMode    string   `yaml:"availabilityMode,omitempty"`
+        SearchDistance      string   `yaml:"search_distance,omitempty"`
+        Postal              string   `yaml:"postal,omitempty"`
+        SearchNearby        string   `yaml:"searchNearby,omitempty"`
+        NearbyAreas         []string `yaml:"nearbyAreas,omitempty"`
+        MinBedrooms         string   `yaml:"min_bedrooms,omitempty"`
+        MaxBedrooms         string   `yaml:"max_bedrooms,omitempty"`
+        MinBathrooms        string   `yaml:"min_bathrooms,omitempty"`
+        MaxBathrooms        string   `yaml:"max_bathrooms,omitempty"`
+        MinSqft             string   `yaml:"minSqft,omitempty"`
+        MaxSqft             string   `yaml:"maxSqft,omitempty"`
+        PetsCat             string   `yaml:"pets_cat,omitempty"`
+        PetsDog             string   `yaml:"pets_dog,omitempty"`
+        IsFurnished         string   `yaml:"is_furnished,omitempty"`
+        Wheelchaccess       string   `yaml:"wheelchaccess,omitempty"`
+        HousingType         []string `yaml:"housing_type,omitempty"`
+        Laundry             []string `yaml:"laundry,omitempty"`
+        Parking             []string `yaml:"parking,omitempty"`
+    } `yaml:"Query"`
 }
 
 func main() {
     c := Config{}
     c.getConf()
-    fmt.Println(c)
-    //m := getConfigMap()
-    createQueryString(c)
-    //fmt.Println(q)
+
+    q := createQueryString(c)
+    fmt.Println(q)
 }
 
 func (c *Config) getConf() *Config {
@@ -46,58 +72,23 @@ func (c *Config) getConf() *Config {
     if err != nil {
         log.Fatalf("Unmarshal: %v", err)
     }
-    fmt.Printf("%v", c)
 
     return c
 }
 
-//func getConfigMap() map[string]map[string]string {
-//    // Since the nested values are also a map, need to do some mapception
-//    m := make(map[string]map[string]string)
-//
-//    yamlFile, err := ioutil.ReadFile("Config.yaml")
-//    if err != nil {
-//        log.Printf("yamlFile.Get err #%s ", err)
-//    }
-//
-//    // Dynamically unmarshal key/value pairs into our map of string maps
-//    err = yaml.Unmarshal(yamlFile, m)
-//    if err != nil {
-//        log.Fatal(err)
-//    }
-//
-//    return m
-//}
-
 func createQueryString(c Config) *url.URL {
-    u, err := url.Parse(c.Search.URL)
-    if err != nil {
-        log.Fatal(err)
-    }
+    u := new(url.URL)
+    host := c.Search.Location + "." + c.Search.URL
+    u.Scheme = c.Search.Scheme
+    u.Host = host
 
-    // Create an empty query string that we Set() key/value pairs on; results in "key=value&key=value&..."
-//    q := u.Query()
-//    for k, v := range m["Query"] {
-//        // TODO: Handle sequence of values so that they result in a series of duplicate keys, e.g. housing=1&housing=2&housing=3
-//        if strings.Contains(v, ",") { // Tried giving values in YAML file like: "housing: 1,2"
-//            s := strings.Split(v, ",")
-//            for i, j := range s {
-//                q.Set(k, j)
-//                fmt.Println(i)
-//                fmt.Println(j)
-//                // Guess there'd be a 'continue' here or something
-//            }
-//            fmt.Println(k)
-//            fmt.Println(v)
-//        }
-//        q.Set(k, v) // But what to do about this if it's a sequence...
-//        // Would rather set value in config like:
-//        // housing: [1 2]
-//        // Problem is map explicitly contains strings, so results in: "line 63: cannot unmarshal !!seq into string"
-//        // Tried changing map to map[string]map[string]interface{} but then it gets messy with type conversions etc.
-//    }
-//
-//    u.RawQuery = q.Encode()
-//
+    form := url.Values{}
+
+    encoder := schema.NewEncoder()
+    encoder.SetAliasTag("yaml")
+    encoder.Encode(c.Query, form)
+
+    u.RawQuery = form.Encode()
+
     return u
 }
